cd97de357264e138bb9331d7977664c75eea3d79
brenoasantos <brenoaas2@gmail.com>
2024-03-15 04:35:36 -0300

    first commit

--- index.js
+++ index.js
@@ -1,68 +1,92 @@
-const { chromium } = require('playwright');
-const readline = require('readline');
+const { chromium } = require('playwright'); // Import library 'Chromium' from the 'Playwright' packet to create an instance of Chromium, browser controlled by Playwright
+const readline = require('readline'); // Import the module 'readline', used to create a text input reading interface in the terminal
 
-// Solicitar a palavra-chave ao usuário antes de qualquer código ser executado
-const input = readline.createInterface({
- input: process.stdin,
- output: process.stdout
+const input = readline.createInterface({ // Prompt user for keyword
+   input: process.stdin,
+   output: process.stdout
 });
 
-input.question('Digite a palavra-chave para a busca: ', async (keyword) => {
- const browser = await chromium.launch({ headless: false });
- const page = await browser.newPage();
+input.question('Enter the keyword for the search: ', async (keyword) => {
+   // Initialize Chromium and set the headless parameter: false, which allows graphical visualization of the code step by step in the started Chromium instance
+   const browser = await chromium.launch({ headless: false });
+   const page = await browser.newPage(); // Create a new page in the browser from the launched instance
+   await page.goto('https://www.amazon.com');  // Access Amazon
+   await page.waitForTimeout(2000); // Wait for page elements to load
+   await page.click('#glow-ingress-block'); // Click on the button to edit address
+   await page.waitForTimeout(2000); // Wait for page elements to load
+   await page.fill('input[class="GLUX_Full_Width a-declarative"]', '11001'); // Fill in the zip code provided 11001
+   await page.click('#GLUXZipUpdate-announce'); // Click on the apply button
+   await page.waitForTimeout(3000); // Wait for page elements to load
+   await page.locator('span.a-button-text#GLUXConfirmClose-announce').last().click({ force: true }); // Click on the Done button
+   await page.waitForTimeout(3000); // Wait for page elements to load
+   // Perform a search with the user-supplied keyword
+   await page.fill('input[name="field-keywords"]', keyword); // Fill in the search bar with the keyword provided by the user
+   await page.click('span[class="nav-search-submit-text nav-sprite nav-progressive-attribute"]'); //Click on the search button
+   await page.waitForTimeout(3000); // Wait for page elements to load
 
- // Acessar 'amazon.com'
- await page.goto('https://www.amazon.com');
+   const nonSponsoredProduct = await page.locator('div[data-cy="title-recipe"]:not(:has(div[class="a-row a-spacing-micro"]))'); // References the first non-sponsored product
 
- // Clicar no botão 'deliver to'
- await page.click('#glow-ingress-block');
+   if (await nonSponsoredProduct.count() > 0) { // Check if there is at least one non-sponsored product
+      // Get the link to the first non-sponsored product
+      const productLink = await nonSponsoredProduct.first().evaluate(el => el.querySelector('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]').href);
+      await page.goto(productLink); // Access the link
 
- // Inserir código zip = 11001
- await page.waitForTimeout(2000);
- await page.fill('input[class="GLUX_Full_Width a-declarative"]', '11001');
+      console.log(`\nResult for '${keyword}':\n`); // Inform about the result obtained from the keyword provided by the user
 
- // Clicar em done
- await page.click('#GLUXZipUpdate-announce');
+      // Extract title, price, units sold, rating, number of reviews and feature bullets
+      const title = await page.$eval('span.a-size-large.product-title-word-break', el => el.textContent);
+      const priceWhole = await page.$eval('span.a-price-whole', el => el.textContent);
+      const priceFraction = await page.$eval('span.a-price-fraction', el => el.textContent);
+      const price = `${priceWhole}${priceFraction}`;
+      console.log(`Title: ${title.trim()}`); // Print product title on terminal
+      console.log(`Price: $${price}`); // Print product price on terminal
 
- // Clicar em continue
- await page.waitForTimeout(3000);
- await page.locator('span.a-button-text#GLUXConfirmClose-announce').last().click({ force: true });
- await page.waitForTimeout(3000);
+      try { // Try to access the element that contains the number of units sold
+            const unitsSold = await page.$eval('span[id="social-proofing-faceout-title-tk_bought"]', el => el.textContent);
+            const cleanUnitsSold = unitsSold.trim().split(' ')[0].replace('K', ',000'); // Make it more presentable
+            console.log(`Units sold: ${cleanUnitsSold} past month`); // Print units sold of the product in the previous month on the terminal
+      } catch (error) { // If it is not possible to access the element, inform that there is no information about sales
+            console.log("Units sold: no information");
+      }
 
- // Realizar uma busca com a palavra-chave fornecida pelo usuário
- await page.fill('input[name="field-keywords"]', keyword);
- await page.click('span[class="nav-search-submit-text nav-sprite nav-progressive-attribute"]');
- await page.waitForTimeout(3000);
+      try { // Try to access the element that contains the product rating 
+            const ratingText = await page.$eval('span[data-hook="rating-out-of-text"]', el => el.textContent);
+            console.log(`Rating: ${ratingText} stars`); // Print product rating on terminal
+      } catch (error) { // If it is not possible to access the element, inform that the product has not yet been rated
+            console.log("Rating: product not yet rated");
+      }
 
- // Encontrar o primeiro produto não patrocinado
- const nonSponsoredProduct = await page.locator('div[data-cy="title-recipe"]:not(:has(div[class="a-row a-spacing-micro"]))');
+      try { // Try to access the element that contains the product number of reviews 
+            const numReviews = await page.$eval('span[data-hook="total-review-count"]', el => el.textContent);
+            const numReviewsClean = numReviews.replace(/[^0-9,]/g, ''); // Make it more presentable
+            console.log(`Number of reviews: ${numReviewsClean}`); // Print product number of reviews on terminal
+      } catch (error) { // If it is not possible to access the element, inform that the product has not yet been reviewed
+            console.log("Number of reviews: product not yet reviewed");
+      }
+      // Print the resource markers with a line break so that the information is below the rest, as the volume of content is greater than the rest
+      console.log('Feature bullets:\n');
 
- // Obter o link do produto não patrocinado
- if (await nonSponsoredProduct.count() > 0) {
-    const productLink = await nonSponsoredProduct.first().evaluate(el => el.querySelector('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]').href);
-    // Navegar para o link do produto
-    await page.goto(productLink);
- } else {
-    console.log('Nenhum produto não patrocinado encontrado.');
- }
- await page.waitForTimeout(3000);
+      // Initialize a structure that is modifiable as feature bullets do not have as uniform a structure as other product information
+      let featureBullets = '';
+      try { // Try to access the element containing the resource markers header ('About this item') because it is not mandatory to have
+            const header = await page.$eval('div.a-section.a-spacing-medium.a-spacing-top-small h1', el => el.textContent);
+            featureBullets += `${header.trim()}\n\n`;
+      } catch (error) {} // Simply do nothing if the element does not exist
+      // Dynamically add feature bullets divided into topics like on website to make the terminal content more presentable
+      featureBullets += await page.$$eval('div.a-section.a-spacing-medium.a-spacing-top-small ul li', listItems => {
+            return listItems.map(item => {
+               return '- ' + item.textContent.trim();
+            }).join('\n');
+      });
 
- // Fechar a interface de leitura de linha
- input.close();
+      console.log(featureBullets); // Print product feature bullets on terminal
 
- await browser.close();
+   } else { // If no (non-sponsored) product is found, inform
+      console.log('No products found');
+   }
+
+   await page.waitForTimeout(3000); // Wait a bit so it doesn't close suddenly
+   await page.close(); // Close the page
+   await browser.close(); // Close the browser
+   input.close(); // Close the reading interface that was opened to obtain user input
 });
-
- // Entrar no primeiro item e pegar as informações
- //const title = await firstNonSponsoredItem.evaluate('span.a-size-medium', el => el.textContent);
- //const aboutThisItem = await firstItem.evaluate('div.a-section.a-spacing-none', el => el.textContent);
- //const price = await firstItem.evaluate('span.a-price-whole', el => el.textContent);
- //const unitsSold = await firstItem.evaluate('span.a-size-base', el => el.textContent);
- //const rating = await firstItem.evaluate('span.a-icon-alt', el => el.textContent);
-
- // Mostrar as informações no terminal
- //console.log(`Title: ${title}`);
- //console.log(`About this item: ${aboutThisItem}`);  
- //console.log(`Price: ${price}`);
- //console.log(`Units sold: ${unitsSold}`);
- //console.log(`Rating: ${rating}`);
